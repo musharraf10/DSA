6a757a24f82e5ae2cff83b04c318386f35f95a2f
musharraf10 <skmusharaf00@gmail.com>
2024-12-13 17:11:56 +0530

    Adding two new files on 13/12/24

---
+++ Advanced/MatrixTranspose.java
@@ -0,0 +1,40 @@
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        transposeMatrix(matrix);
+    }
+
+    public static void transposeMatrix(int matrix[][]) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        System.out.println("Original Matrix:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        int transpose[][] = new int[col][row];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("\nTransposed Matrix:");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
