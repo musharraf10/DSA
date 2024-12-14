bb4c1791ad3b158c8a6ba81bbb21b70e555c4357
musharraf10 <skmusharaf00@gmail.com>
2024-12-14 19:33:34 +0530

    Added One File LN 14/12/24

---
+++ Basics/LargestNumberLeet179.java
@@ -0,0 +1,25 @@
+class LargestNumberLeet179 {
+    public String largestNumber(int[] nums) {
+        // Convert integers to strings
+        String[] pens = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            pens[i] = String.valueOf(nums[i]);
+        }
+
+        // Sort strings using custom comparator
+        Arrays.sort(pens, (a, b) -> (b + a).compareTo(a + b));
+
+        // If the largest number is '0', return "0"
+        if (pens[0].equals("0")) {
+            return "0";
+        }
+
+        // Build the result
+        StringBuilder result = new StringBuilder();
+        for (String pen : pens) {
+            result.append(pen);
+        }
+
+        return result.toString();
+    }
+}
