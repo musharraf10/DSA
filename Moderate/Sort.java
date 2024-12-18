7ef747c4f5fa7f1a9195ab0a40834eb30eb2a9e6
musharraf10 <skmusharaf00@gmail.com>
2024-12-18 12:23:35 +0530

    All codes corrected

--- Moderate/Sort.java
+++ Moderate/Sort.java
@@ -1,40 +1,42 @@
 public class Sort {
-    public static void BubbleSort(int arr[]){
+    public static void BubbleSort(int arr[]) {
         int len = arr.length;
-        for(int turn = 0 ; turn < len ; turn++){
-            for(int j = 0 ; j < len-1-turn ; j++){
-                if(arr[j] > arr[j+1]){
+        for (int turn = 0; turn < len; turn++) {
+            for (int j = 0; j < len - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
-    public static void SelectionSort(int arr[]){
+    public static void SelectionSort(int arr[]) {
         int len = arr.length;
-        for(int i = 0 ; i < len ; i++){
+        for (int i = 0; i < len; i++) {
             int minPos = i;
-            for(int j = i+1 ; j < len ; j++){
-                
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < arr[minPos]) {
+                    minPos = j;
+                }
             }
+            int temp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = temp;
         }
 
     }
 
-    public static void printArr(int arr[]){
-        for(int i = 0 ; i < arr.length ; i++){
-            System.out.print(arr[i]+" ");
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = {4,1,5,3,2};
+        int arr[] = { 4, 1, 5, 3, 2 };
         BubbleSort(arr);
         printArr(arr);
     }
 }
-
-
-
