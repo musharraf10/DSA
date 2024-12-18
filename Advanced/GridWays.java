318506923932d684b2fb6ffa2b78513fbfd9be55
musharraf10 <skmusharaf00@gmail.com>
2024-12-18 12:25:01 +0530

    All codes corrected

--- Advanced/GridWays.java
+++ Advanced/GridWays.java
@@ -1,16 +1,17 @@
 public class GridWays {
-    public static int GridWays(int i , int  j , int n , int m){
-        if(i == n-1 && j == m-1){
+    public static int GridWays1(int i, int j, int n, int m) {
+        if (i == n - 1 && j == m - 1) {
             return 1;
-        }else if(i == n || j == n){
+        } else if (i == n || j == n) {
             return 0;
         }
-        int w1 = GridWays(i+1,j,n,m);
-        int w2 = GridWays(i, j+1, n, m);
-        return w1+w2;
+        int w1 = GridWays1(i + 1, j, n, m);
+        int w2 = GridWays1(i, j + 1, n, m);
+        return w1 + w2;
     }
+
     public static void main(String[] args) {
-        int n = 2 , m = 2;
-        System.out.println(GridWays(0,0,n,m));
+        int n = 2, m = 2;
+        System.out.println(GridWays1(0, 0, n, m));
     }
 }
