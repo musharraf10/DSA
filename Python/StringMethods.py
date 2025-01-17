694944c8d085436d0db15a6c988df1352324928f
musharraf10 <skmusharaf00@gmail.com>
2025-01-17 22:04:06 +0530

    Python String File Added

--- Python/StringMethods.py
+++ Python/StringMethods.py
@@ -1,3 +1,33 @@
-str = "Musharaf"
+str1 = "shaik musharaf"
 
-print(str[1:4])
+print() 
+print()
+print(str1[1:4])
+#hai
+
+#Upper
+upperStr = str1.upper()
+print(f"This is Upper String : {upperStr}")
+
+#Lower
+
+print(f"This is Lower String : {str1.lower()}")
+
+#swapcase
+
+print("Swap case : ", str1.swapcase())
+
+#capitalize
+
+print("Capitalize : ", str1.capitalize())
+
+#Title
+
+print("Title of String :", str1.title())
+
+#count
+
+print("Count of A -> ",str1.count('a'))
+
+
+
