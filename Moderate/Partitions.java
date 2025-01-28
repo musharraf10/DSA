955b0ff8ed7007274e4846052710be82af563c89
musharraf10 <skmusharaf00@gmail.com>
2025-01-28 23:13:06 +0530

    Added HTML files

--- Moderate/Partitions.java
+++ Moderate/Partitions.java
@@ -3,21 +3,17 @@ class Partitions {
         int n = nums.length;
         int count = 0;
 
-        // Compute the total sum of the array
         int totalSum = 0;
         for (int num : nums) {
             totalSum += num;
         }
 
-        // Variable to track the sum of the left partition
         int leftSum = 0;
 
-        // Iterate through the array to check partitions
         for (int i = 0; i < n - 1; i++) {
             leftSum += nums[i];
             int rightSum = totalSum - leftSum;
 
-            // Check if the difference is even or zero
             if ((rightSum - leftSum) % 2 == 0 || (rightSum - leftSum) == 0) {
                 count++;
             }
