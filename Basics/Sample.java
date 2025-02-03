0c767ee18ac299ce50bf829588f8498faeb0078f
musharraf10 <skmusharaf00@gmail.com>
2025-02-03 22:39:40 +0530

    Added one file

---
+++ Basics/Sample.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Sample {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        number = 5;
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+
+    }
+}
